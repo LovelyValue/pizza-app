ca2a65fc3bcdb30bbaf8abf4f7b69fe5b05c8d06
Lovely.Value <aid.l.v.esd@gmail.com>
2025-02-11 17:14:29 +0300

    создание кнопки и input

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -3,4 +3,5 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 //Интерфейс для Button props
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode;
+	appearance?: 'small' | 'big';
 }
