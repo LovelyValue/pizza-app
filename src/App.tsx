aa3256b10ae0dcf960d63912db4a4f8545824305
Lovely.Value <aid.l.v.esd@gmail.com>
2025-02-11 18:23:09 +0300

    создание роутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+import { Menu } from './pages/Menu/Menu';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -17,6 +21,15 @@ function App() {
 				Кнопка
 			</Button>
 			<Input placeholder='Email' />
+			<div>
+				<a href='/'>Меню</a>
+				<a href='/cart'>Корзина</a>
+			</div>
+			<Routes>
+				<Route path='/' element={<Menu />} />
+				<Route path='cart' element={<Cart />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
 		</>
 	);
 }
