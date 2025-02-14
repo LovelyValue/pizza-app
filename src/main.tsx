85f2e3b7719dd7867c94683ad162426c86289b6e
Lovely.Value <aid.l.v.esd@gmail.com>
2025-02-14 17:58:21 +0300

    outlet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import { Layout } from './layout/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
 import { Menu } from './pages/Menu/Menu.tsx';
@@ -9,12 +10,19 @@ import { Menu } from './pages/Menu/Menu.tsx';
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Menu />,
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+		],
 	},
-	{
-		path: '/cart',
-		element: <Cart />,
-	},
+
 	{
 		path: '*',
 		element: <Error />,
