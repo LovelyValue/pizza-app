9800435637b540f01ab44c1316cd593643f7e789
Lovely.Value <aid.l.v.esd@gmail.com>
2025-03-07 18:22:46 +0300

    slice пользователя

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user.slice';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		user: userReducer, // Передаём userSlice.reducer
+	},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
