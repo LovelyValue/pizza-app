cda2df47ebe2810ead0f09b2d2dfc72c6c15d861
Lovely.Value <aid.l.v.esd@gmail.com>
2025-03-11 17:01:42 +0300

    профиль пользователя

--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -2,7 +2,9 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { PREFIX } from '../helpers/API';
 import { LoginResponse } from '../interfaces/auth.interface';
+import { Profile } from '../interfaces/user.interface';
 import { loadState } from './storage';
+import { RootState } from './store';
 
 export const JWT_PERSISTENT_STATE = 'userData';
 
@@ -13,6 +15,7 @@ export interface UserPersistentState {
 export interface UserState {
 	jwt: string | null;
 	loginErrorMessage?: string;
+	profile?: Profile;
 }
 
 const initialState: UserState = {
@@ -40,6 +43,20 @@ export const login = createAsyncThunk(
 	}
 );
 
+export const getProfile = createAsyncThunk<Profile, void, { state: RootState }>(
+	'user/profile',
+	async (_, thunkApi) => {
+		const jwt = thunkApi.getState().user.jwt;
+		const { data } = await axios.get<Profile>(`${PREFIX}/user/profile`, {
+			headers: {
+				Authorization: `Bearer ${jwt}`,
+			},
+		});
+
+		return data;
+	}
+);
+
 export const userSlice = createSlice({
 	name: 'user',
 	initialState,
@@ -61,6 +78,9 @@ export const userSlice = createSlice({
 		});
 		builder.addCase(login.rejected, (state, action) => {
 			state.loginErrorMessage = action.payload as string;
+		});
+		builder.addCase(getProfile.fulfilled, (state, action) => {
+			state.profile = action.payload;
 		});
 	},
 });
